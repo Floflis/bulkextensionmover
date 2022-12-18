02f7d49ccbc246be0d07e5236122efc6bb118258
Daniella Mesquita <mesquitadaniella@proton.me>
2022-12-17 22:59:07 -0300

    Complement previous commit

--- test-1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for f in /path/to/*.html
-do
- 
-	echo "Removing password for pdf file - $f"
-	# always "double quote" $f to avoid problems
-	/path/to/command --option "$f"
-done
-#based from https://www.cyberciti.biz/faq/bash-loop-over-file/
--- test-3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for file in ./*.html #hardcoded
-do
-	mv "$file" "Pages 10" #hardcoded
-done
-#based from https://www.cyberciti.biz/faq/bash-loop-over-file/
-#first tested in /home/daniell/Downloads
